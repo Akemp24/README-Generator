87aca0179187af1b3e81465dad709929b7bcd055
Asiak <asiakemp24@gmail.com>
2023-06-15 18:07:55 -0400

    finished making renderLicenseBadge function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,17 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // if (license && license !== 'None') {
-  //   return ``
-  // }
+  if (license && license !== 'None') {
+    return `[![License](https://img.shields.io/badge/License-${license}-blue.svg)](https://choosealicense.com/licenses/${license}/)`;
+  }
+  return '';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -27,7 +30,7 @@ function generateMarkdown(data) {
   [License](#license)
   [Badges](#badges)
   [Features](#features)
-  
+
   ## Installation
 
   ## Usage
