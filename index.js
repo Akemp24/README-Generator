1e00dbdacc33479577da24aa0926774366bbcde7
Asiak <asiakemp24@gmail.com>
2023-06-15 17:24:21 -0400

    made only one question so I can test to see if it works will add later after making function

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type:'input',
+        message:'What is the title?',
+        name:'READMe Title',
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+}
 
 // TODO: Create a function to initialize app
 function init() {}
